17e2840834d779a9729ffad9d390c75749a429c6
GRIMAUD-Estelle-2326056aa <estelle.grimaud@etu.univ-amu.fr>
2024-05-23 08:39:10 +0200

    ajout TP3

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,18 +1,47 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
 
+    @FXML
+    private BorderPane borderpane;
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    private HBox hbox;
+
+    @FXML
+    private Label counterLabel= new Label("0");
+
+    @FXML
+    private Button decrementButton;
+
+    @FXML
+    private Button incrementButton;
+
     int counter = 0;
 
     public void increment() {
+        counter++;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     public void decrement() {
+        counter--;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     @Override
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
-module tp.intro.javafx {
+open module tp.intro.javafx {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.fxml;
-    exports com.example.partie2;
-    opens com.example.partie3 to javafx.fxml;
-    exports com.example.partie3;
 }
